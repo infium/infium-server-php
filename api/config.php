a917555ca42a38a8033e99b8584c39cdb5ba3c13
Marcus Hammar <marcus@infium.se>
2017-09-06 20:09:52 +0200

    Move auditTrailLog from config.php to its own file

--- api/config.php
+++ api/config.php
@@ -5,7 +5,7 @@ require('functionDatabase.php');
 require('functionExtendedLogging.php');
 require('functionUserValidation.php');
 require('functionMatchOpenItems.php');
-
+require('functionAuditTrailLog.php');
 $now = time();
 
 $baseUrl = 'https://infium-eu.appspot.com/api/';
@@ -59,39 +59,6 @@ function decimalFormat($value){
 	}else{
 		return $valueFormatted;
 	}
-}
-
-function auditTrailLog($pdo, $table, $tableId, $operation){
-	global $now;
-	global $testing;
-	
-	if (!preg_match('/^[a-zA-Z]{1,64}$/', $table)){
-		throw new Exception('The table must consist of the letters A-Z or a-z. No other characters.');
-	}
-	
-	if (($operation == 'INSERT')||($operation == 'UPDATE')){
-		$data = dbPrepareExecute($pdo, 'SELECT * FROM '.$table.' WHERE Id=?', array($tableId));
-		
-		foreach($data[0] as $key => $value){
-			if (($key == 'PasswordSalt')||($key == 'PasswordEncrypted')){
-				$data2[$key] = '*';
-			}else{
-				$data2[$key] = $value;
-			}
-		}
-		
-		$dataJSON = json_encode($data2);
-	}else{
-		$dataJSON = NULL;
-	}
-	
-	if (isset($testing) && $testing == True){
-		$user = 0;
-	}else{
-		$user = getUser($pdo);
-	}
-	
-	dbPrepareExecute($pdo, "INSERT INTO AuditTrail (`Table`, `TableId`, `Operation`, `Data`, `Time`, `User`, `IP`) VALUES (?, ?, ?, ?, ?, ?, ?)", array($table, $tableId, $operation, $dataJSON, date("Y-m-d H:i:s", $now), $user, $_SERVER['REMOTE_ADDR']));
 }
 
 function sendMessageToClient($message){
---
+++ api/functionAuditTrailLog.php
@@ -0,0 +1,34 @@
+<?php
+function auditTrailLog($pdo, $table, $tableId, $operation){
+    global $now;
+    global $testing;
+
+    if (!preg_match('/^[a-zA-Z]{1,64}$/', $table)){
+        throw new Exception('The table must consist of the letters A-Z or a-z. No other characters.');
+    }
+
+    if (($operation == 'INSERT')||($operation == 'UPDATE')){
+        $data = dbPrepareExecute($pdo, 'SELECT * FROM '.$table.' WHERE Id=?', array($tableId));
+
+        foreach($data[0] as $key => $value){
+            if (($key == 'PasswordSalt')||($key == 'PasswordEncrypted')){
+                $data2[$key] = '*';
+            }else{
+                $data2[$key] = $value;
+            }
+        }
+
+        $dataJSON = json_encode($data2);
+    }else{
+        $dataJSON = NULL;
+    }
+
+    if (isset($testing) && $testing == True){
+        $user = 0;
+    }else{
+        $user = getUser($pdo);
+    }
+
+    dbPrepareExecute($pdo, "INSERT INTO AuditTrail (`Table`, `TableId`, `Operation`, `Data`, `Time`, `User`, `IP`) VALUES (?, ?, ?, ?, ?, ?, ?)", array($table, $tableId, $operation, $dataJSON, date("Y-m-d H:i:s", $now), $user, $_SERVER['REMOTE_ADDR']));
+}
+?>
