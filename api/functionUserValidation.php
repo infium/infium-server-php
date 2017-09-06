9bda540c5c7663ab24a0a05ed9eb8a4c414f834c
Marcus Hammar <marcus@infium.se>
2017-09-06 20:01:42 +0200

    Move user validation functionality from config.php to its own file

---
+++ api/functionUserValidation.php
@@ -0,0 +1,92 @@
+<?php
+function getUser($pdo){
+    $results = dbPrepareExecute($pdo, 'SELECT UserId FROM UserToken WHERE Token=?', array($_SERVER['HTTP_X_CLIENT_LOGIN_TOKEN']));
+
+    if (count($results) != 1){
+        throw new Exception('User token cannot be found');
+    }
+
+    return $results[0]['UserId'];
+}
+
+function getUserName($pdo, $userId){
+    if ($userId == 0){
+        return 'Initial system setup';
+    }
+
+    $results = dbPrepareExecute($pdo, 'SELECT Name FROM User WHERE Id=?', array($userId));
+
+    if (count($results) != 1){
+        throw new Exception('User cannot be found');
+    }
+
+    return $results[0]['Name'];
+}
+
+function checkUserAccess($resourceName = NULL, $throwException = False){
+    $pdo = createPdo();
+
+    $stmt = $pdo->prepare('SELECT COUNT(*) as MatchingUsers FROM UserToken WHERE Token=?');
+    $stmt->execute(array($_SERVER['HTTP_X_CLIENT_LOGIN_TOKEN']));
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($results[0]['MatchingUsers'] != 1){
+
+        if ($throwException == True){
+            throw new Exception('User token cannot be found');
+        }else{
+            $response['Response'] = 'LocalActions';
+            $response['Data'][0]['Action'] = 'Logout';
+            $response['Data'][1]['Action'] = 'MessageFlash';
+            $response['Data'][1]['Message'] = 'User is not logged in';
+
+            header('Content-type: application/json');
+            echo json_encode($response,JSON_PRETTY_PRINT);
+
+            exit();
+        }
+    }
+
+    if ($resourceName != NULL){
+        $stmt2 = $pdo->prepare('SELECT UserId FROM UserToken WHERE Token=?');
+        $stmt2->execute(array($_SERVER['HTTP_X_CLIENT_LOGIN_TOKEN']));
+        $results2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+
+        $results3 = dbPrepareExecute($pdo, 'SELECT Access FROM User WHERE Id=?', array($results2[0]['UserId']));
+
+        $accessArray = json_decode($results3[0]['Access'], TRUE);
+
+        $accessGranted = False;
+
+        foreach($accessArray as $access){
+            if ($resourceName == $access){
+                $accessGranted = True;
+            }
+        }
+
+        if ($accessGranted == False){
+            if ($throwException == True){
+                throw new Exception('User has no access to this');
+            }else{
+                $response['Response'] = 'LocalActions';
+                $response['Data'][0]['Action'] = 'MessageFlash';
+                $response['Data'][0]['Message'] = 'User has no access to this';
+
+                header('Content-type: application/json');
+                echo json_encode($response,JSON_PRETTY_PRINT);
+
+                exit();
+            }
+        }
+    }
+}
+
+function checkUserAccessBoolean($resourceName = NULL) {
+    try {
+        checkUserAccess($resourceName, True);
+    } catch (Exception $e) {
+        return false;
+    }
+    return true;
+}
+?>
