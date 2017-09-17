24e3ad1e432e370f3ce1ffed5d77bfbb12522065
Marcus Hammar <marcus@infium.se>
2017-09-17 15:58:23 +0200

    Add license information to all files

--- api/functionMatchOpenItems.php
+++ api/functionMatchOpenItems.php
@@ -1,4 +1,20 @@
 <?php
+/*
+ * Copyright 2012-2017 Infium AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 function matchOpenItems ($pdo, $date, $account, $subAccount, $reference){
     $sum = 0;
     $results = dbPrepareExecute($pdo, 'SELECT Id, Amount FROM GeneralLedgerAccountBookingRow WHERE AccountNumber=? AND SubAccountNumber=? AND ClearingReference=? AND ClearingDate IS NULL', array($account, $subAccount, $reference));
