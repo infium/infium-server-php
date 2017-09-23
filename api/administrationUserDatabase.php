830838f6ff98622bbf71f2d2b002d23203b8f1f3
Marcus Hammar <marcus@infium.se>
2017-09-23 10:24:55 +0200

    Create the UI only once in the user database screen

--- api/administrationUserDatabase.php
+++ api/administrationUserDatabase.php
@@ -30,8 +30,6 @@ $stmt = $pdo->prepare('SELECT Id,Name FROM User ORDER BY Name ASC');
 $stmt->execute(array());
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$ui = new UserInterface();
-
 foreach ($results as $row){
     $ui->addLabelValueLink($row['Name'], NULL, 'GET', $baseUrl.'administrationUserDatabaseView2.php?Id='.$row['Id'], NULL, $titleBarColorAdministrationUserDatabase);
 }
